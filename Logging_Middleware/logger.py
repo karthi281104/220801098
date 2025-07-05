68d82d7656173987bd5120dd10c0061093b03384
karthi281104 <karthikeyana281104@gmail.com>
2025-07-05 11:51:52 +0530

    latest

--- Logging_Middleware/logger.py
+++ Logging_Middleware/logger.py
@@ -1,7 +1,7 @@
+
 import requests
 import json
-
-BEARER_TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiYXVkIjoiaHR0cDovLzIwLjI0NC41Ni4xNDQvZXZhbHVhdGlvbi1zZXJ2aWNlIiwiZW1haWwiOiIyMjA4MDEwOThAcmFqYWxha3NobWkuZWR1LmluIiwiZXhwIjoxNzUxNjk0MzQ1LCJpYXQiOjE3NTE2OTM0NDUsImlzcyI6IkFmZm9yZCBNZWRpY2FsIFRlY2hub2xvZ2llcyBQcml2YXRlIExpbWl0ZWQiLCJqdGkiOiJlZGE5ZWQwNy0wOWFmLTQwMmYtYTBiNS1lMjZkZGI3MTNlNWEiLCJsb2NhbGUiOiJlbi1JTiIsIm5hbWUiOiJrYXJ0aGlrZXlhbiBhIiwic3ViIjoiNDIwYjU4ZTUtNmYwYy00NjgzLThjN2QtY2NmZjIxODk3ZTRhIn0sImVtYWlsIjoiMjIwODAxMDk4QHJhamFsYWtzaG1pLmVkdS5pbiIsIm5hbWUiOiJrYXJ0aGlrZXlhbiBhIiwicm9sbE5vIjoiMjIwODAxMDk4IiwiYWNjZXNzQ29kZSI6ImNXeWFYVyIsImNsaWVudElEIjoiNDIwYjU4ZTUtNmYwYy00NjgzLThjN2QtY2NmZjIxODk3ZTRhIiwiY2xpZW50U2VjcmV0IjoieHhWUXBja1BFc3JheXdrUyJ9.grfkt880o-T1kuu7WtnfMhdSKFW9TolR8WDmk1ypY5s'
+BEARER_TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiYXVkIjoiaHR0cDovLzIwLjI0NC41Ni4xNDQvZXZhbHVhdGlvbi1zZXJ2aWNlIiwiZW1haWwiOiIyMjA4MDEwOThAcmFqYWxha3NobWkuZWR1LmluIiwiZXhwIjoxNzUxNjk3MzEyLCJpYXQiOjE3NTE2OTY0MTIsImlzcyI6IkFmZm9yZCBNZWRpY2FsIFRlY2hub2xvZ2llcyBQcml2YXRlIExpbWl0ZWQiLCJqdGkiOiI2ZTdmZGU1MS00MjI1LTQ5NWQtODU5OC0zZGM3YTBjNTBiMjQiLCJsb2NhbGUiOiJlbi1JTiIsIm5hbWUiOiJrYXJ0aGlrZXlhbiBhIiwic3ViIjoiNDIwYjU4ZTUtNmYwYy00NjgzLThjN2QtY2NmZjIxODk3ZTRhIn0sImVtYWlsIjoiMjIwODAxMDk4QHJhamFsYWtzaG1pLmVkdS5pbiIsIm5hbWUiOiJrYXJ0aGlrZXlhbiBhIiwicm9sbE5vIjoiMjIwODAxMDk4IiwiYWNjZXNzQ29kZSI6ImNXeWFYVyIsImNsaWVudElEIjoiNDIwYjU4ZTUtNmYwYy00NjgzLThjN2QtY2NmZjIxODk3ZTRhIiwiY2xpZW50U2VjcmV0IjoieHhWUXBja1BFc3JheXdrUyJ9.jKJmp7dsMbVtrBHuHuZD6bF2ycwPR1u1qxNW93u-ucM'
 
 def Log(stack: str, level: str, package: str, message: str):
     log_url = "http://20.244.56.144/evaluation-service/logs"
@@ -12,9 +12,9 @@ def Log(stack: str, level: str, package: str, message: str):
 
     body = {
         "stack": "backend",
-        "level": level,
-        "package": package,
-        "message": message
+        "level": "error",
+        "package": "handler",
+        "message": "received string,expected bool"
     }
 
     try:
